e8a88e2c9294b9e4894216cb9b9873bb318358d2
abe <adealba@csumb.edu>
2016-10-02 21:35:58 -0700

    webcam addition

--- CST205project2.py
+++ CST205project2.py
@@ -4,9 +4,11 @@ import cv2
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
-
+test=face_cascade.load('haarcascade_frontalface_default.xml')
+print(test)
 img = cv2.imread('100_0750.jpg')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+print (face_cascade)
 faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 for (x,y,w,h) in faces:
    cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
@@ -16,6 +18,9 @@ for (x,y,w,h) in faces:
 for (ex,ey,ew,eh) in eyes:
    cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
 
-cv2.imshow('100_0750',img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+files = [image for image in os.listdir('.') if os.pathisfile(image) and (image.endswith('jpg') or image.endswith('jpeg'))]
+for f in files:
+	pic = cv2.imread(f)
+	cv2.imshow('100_0750',img)
+	cv2.waitKey(0)
+	cv2.destroyAllWindows()
